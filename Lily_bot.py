2896cf490348eca0cd978484b9190cfc608b6491
AptS:1553 <qdengag@connect.ust.hk>
2026-04-28 17:21:11 +0800

    修改过的版本

--- Lily_bot.py
+++ Lily_bot.py
@@ -5,7 +5,7 @@ import base64
 import nonebot
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Adapter, MessageSegment
-from pixel import lily_generate
+from pixel import lily_generate, dst_generate, scenery_generate
 # from nonebot import on_command
 
 nonebot.init()
@@ -17,13 +17,16 @@ driver.register_adapter(Adapter)
 
 nonebot.load_builtin_plugins("echo")
 
+Help = on_command(cmd = "help", block = True)
 Lily = on_command(cmd = "lily", block = True)
+Dst = on_command(cmd = "dst", block = True)
+Scenery = on_command(cmd = "scenery", block = True)
 
 config = nonebot.get_driver().config
 pixiv_token = getattr(config, "pixiv_refresh_token", None)
 
 @Lily.handle()
-async def handle_function():
+async def handle_lily():
     """
     用来接收 /lily 并回复的函数
     """
@@ -35,7 +38,46 @@ async def handle_function():
         await Lily.send(MessageSegment.image(f"base64://{base64_str}"))
     except Exception as e:
         await Lily.send(f"图片发送失败了，报错信息为{e}")
-
+@Dst.handle()
+async def handle_dst():
+    """
+    用来接收 /dst 并回复的函数
+    """
+    image_path = dst_generate(pixiv_token)
+    try:
+        with open(image_path, "rb") as f:
+            image_bytes = f.read()
+            base64_str = base64.b64encode(image_bytes).decode("utf-8")
+        await Dst.send(MessageSegment.image(f"base64://{base64_str}"))
+    except Exception as e:
+        await Dst.send(f"图片发送失败了，报错信息为{e}")
+@Scenery.handle()
+async def handle_scenery():
+    """
+    用来接收 /scenery 并回复的函数
+    """
+    image_path = scenery_generate(pixiv_token)
+    try:
+        with open(image_path, "rb") as f:
+            image_bytes = f.read()
+            base64_str = base64.b64encode(image_bytes).decode("utf-8")
+        await Scenery.send(MessageSegment.image(f"base64://{base64_str}"))
+    except Exception as e:
+        await Scenery.send(f"图片发送失败了，报错信息为{e}")
+@Help.handle()
+async def help_handle():
+    """
+    一个常见的 /help 功能
+    """
+    string: str = (
+        "这里是 bot-1553,能发图\n"
+        "/lily: 发送一张随机的百合图片\n"
+        "/dst: 发送一张随机的 Don't Starve Together 图片\n"
+        "/scenery: 发送一张随机的风景图片\n"
+        "图片来源: pixiv,已经筛选过 AI 生图了"
+    )
+    await Help.send(string)
 
 if __name__ == "__main__":
     nonebot.run()
+
--- pixel.py
+++ pixel.py
@@ -33,9 +33,9 @@ def lily_appoint(pixiv_id, pixiv_token):
     api.download(illust["image_urls"]["medium"], path = SAVE_DIR, name = file_name)
     return str(full_path)
 
-def lily_generate(pixiv_token):
+def pic_generate(random_number, search_word, pixiv_token):
     """
-    find a random lily pic
+    a function to generate pics from pixiv and save it in the full_path
     """
     BASE_DIR = Path(__file__).resolve().parent
     SAVE_DIR = BASE_DIR / "Lily"
@@ -52,8 +52,8 @@ def lily_generate(pixiv_token):
     api = AppPixivAPI()
     api.auth(refresh_token = pixiv_token)
 
-    random_offset = random.randint(0,100)
-    search_word = "百合 1000users入り -R-18 -漫画"
+    random_offset = random.randint(0, random_number)
+    # search_word = "百合 1000users入り -R-18 -漫画"
 
     json_result = api.search_illust(
         search_word,
@@ -64,22 +64,40 @@ def lily_generate(pixiv_token):
 
     if json_result.illusts:
         # 1. 使用列表推导式，筛选出只有 1 页的作品
-        single_pics = [i for i in json_result.illusts if i.page_count == 1]
+        single_pics = [i for i in json_result.illusts if i.page_count == 1 and i.illust_ai_type == 1] 
         if single_pics:
             # 2. 从筛选后的单图列表中随机选一张
             illust = random.choice(single_pics)
+            api.download(illust["meta_single_page"]["original_image_url"], path = SAVE_DIR, name = file_name)
         else:
             # 如果这一页全是组图（概率很低），就保底选第一张
             illust = json_result.illusts[0]
+            api.download(illust["meta_pages"][0]["image_urls"]["original"], path = SAVE_DIR, name = file_name)
+        if illust:
+            print(illust)
+        else:
+            print("Something wrong here")
+
     else:
         return None
-
-    print(illust["image_urls"]["medium"])
-
-    api.download(illust["image_urls"]["medium"], path = SAVE_DIR, name = file_name)
     return str(full_path)
 
+def lily_generate(pixiv_token):
+    """
+    a function to generate lily-pics by using pic-generate func
+    """
+    return pic_generate(1000, "百合 1000users入り -R-18 -漫画", pixiv_token)
+def dst_generate(pixiv_token):
+    """
+    a function to generate dst-pics by using pic-generate func
+    """
+    return pic_generate(200, "Don'tStarve -R-18 -漫画", pixiv_token)
+def scenery_generate(pixiv_token):
+    """
+    a function to generate scenery-pics by using pic-generate func
+    """
+    return pic_generate(1000, "風景 1000users入り -R-18 -漫画", pixiv_token)
 
 if __name__ == "__main__":
-    # lily_generate()
-    pass
+    scenery_generate("")
+
